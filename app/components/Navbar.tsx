78337530d2bdbbb411f7eac7e38a80703362ebf5
arjunbytes <nagarchunan@gmail.com>
2024-02-29 11:47:01 +0530

    NavBar CREATED

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../public/logo.svg";
+import { usePathname } from "next/navigation";
+import { Search } from "lucide-react";
+
+interface navlinkProps {
+  name: string;
+  href: string;
+}
+
+const navlinks: navlinkProps[] = [
+  { name: "Home", href: "/home" },
+  { name: "Movies", href: "/home/movies" },
+  { name: "TV Shows", href: "/home/tvshows" },
+  { name: "Recently Added", href: "/home/recent" },
+  { name: "My List", href: "/home/user/list" },
+];
+
+export default function Navbar() {
+  const pathName = usePathname();
+  return (
+    <div className="w-full flex max-w-7xl mx-auto items-center justify-between px-5 sm:px-6 lg:px-8 py-5">
+      <div className="flex items-center">
+        <Link href="/home" className="w-32">
+          <Image src={Logo} alt="Netflix" priority />
+        </Link>
+        <ul className="lg:flex gap-x-4 ml-14 hidden">
+          {navlinks.map((link, idx) => (
+            <div key={idx}>
+              {pathName === link.href ? (
+                <li>
+                  <Link
+                    href={link.href}
+                    className="text-sm text-white font-semibold underline"
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              ) : (
+                <li>
+                  <Link
+                    className="text-gray-300 font-normal text-sm"
+                    href={link.href}
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              )}
+            </div>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "../components/Navbar";
+
 export default function Homepage() {
   return (
     <div>
-      <h1>Welcome to Netflix Homepage</h1>
+      <Navbar />
     </div>
   );
 }
